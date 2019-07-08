66e3eec7a46e7c8b5baa0acf96b7d6bd4063f1f9
Xavier Boubert <xavier@boubert.fr>
2019-07-08 15:04:32 +0200

    starting new board design

---
+++ extension/board.js
@@ -0,0 +1,7 @@
+(function() {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,600&display=swap';
+
+  document.head.appendChild(link);
+})();
