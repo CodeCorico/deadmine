6a42136b51df5066137b2c02584ea718619a77e9
Xavier Boubert <xavier@boubert.fr>
2019-07-12 19:29:15 +0200

    3d effect, open/close panel animations

--- extension/boards/board.js
+++ extension/boards/board.js
@@ -4,6 +4,7 @@
   // in seconds
   const REFRESH_INTERVAL = 30;
   const panels = {};
+  const users = {};
   let issueOpened = null;
   let clone = null;
   let issueOpening = false;
@@ -23,11 +24,29 @@
   };
 
   const injectFonts = () => {
-    var link = document.createElement('link');
+    const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,600&display=swap';
 
     window.document.head.appendChild(link);
+  };
+
+  const injectBody = () => {
+    ['top', 'bottom'].forEach((direction) => {
+      const div = document.createElement('div');
+      div.className = `body-effect-${direction}`;
+
+      window.document.body.insertBefore(div, window.document.body.childNodes[0]);
+    });
+  };
+
+  const extractUsers = () => {
+    window.document.querySelectorAll('.project-members > .assignable-user').forEach((userEl) => {
+      const link = userEl.querySelector('a');
+      const name = (link ? link.textContent : userEl.textContent).trim();
+      const src = userEl.querySelector('img').getAttribute('src');
+      users[name] = src;
+    });
   };
 
   const createPanel = (name, src) => {
@@ -39,6 +58,34 @@
     window.document.body.appendChild(iframeContainer);
 
     panels[name] = iframeContainer;
+  };
+
+  const activatePanel = (name) => {
+    if (!panels[name]) {
+      return;
+    }
+
+    const iframe = panels[name].querySelector('iframe');
+    const activate = () => {
+      panels[name].classList.add('open');
+
+      setTimeout(() => panels[name].classList.add('active'), 200);
+    }
+
+    if (iframe.contentDocument.readyState === 'complete') {
+      activate();
+    } else {
+      iframe.contentWindow.onload = () => activate();
+    }
+  };
+
+  const deactivatePanel = (name) => {
+    if (!panels[name]) {
+      return;
+    }
+
+    panels[name].classList.remove('active');
+    setTimeout(() => panels[name].classList.remove('open'), 200);
   };
 
   const destroyPanels = (...names) => {
@@ -82,7 +129,10 @@
     issueOpened.classList.add('hidden');
 
     window.document.querySelector('.agile-board').classList.add('fade');
-    window.document.body.parentNode.classList.add('fixed');
+    // window.document.body.parentNode.classList.add('fixed');
+
+    createPanel('description', `${REDMINE_URL}issues/${id}/?display=description`);
+    createPanel('comments', `${REDMINE_URL}issues/${id}/?tab=comments&display=comments`);
 
     setTimeout(() => {
       clone.classList.add('featured');
@@ -91,12 +141,12 @@
         clone.classList.add('fixed');
 
         setTimeout(() => {
-          createPanel('description', `${REDMINE_URL}issues/${id}/?display=description`);
-          createPanel('comments', `${REDMINE_URL}issues/${id}/?tab=comments&display=comments`);
+          activatePanel('description');
+          activatePanel('comments');
 
           issueOpening = false;
-        }, 350);
-      }, 350);
+        }, 250);
+      }, 250);
     });
   };
 
@@ -107,20 +157,28 @@
 
     issueOpening = true;
 
-    clone.classList.remove('featured');
-    window.document.querySelector('.agile-board').classList.remove('fade');
-
-    destroyPanels('description', 'comments');
+    deactivatePanel('description');
+    deactivatePanel('comments');
 
     setTimeout(() => {
-      issueOpened.classList.remove('hidden');
-      clone.parentNode.removeChild(clone);
-      window.document.body.parentNode.classList.remove('fixed');
-      clone = null;
-      issueOpened = null;
+      clone.classList.remove('fixed');
 
-      issueOpening = false;
-    }, 1000);
+      setTimeout(() => {
+        destroyPanels('description', 'comments');
+        clone.classList.remove('featured');
+        window.document.querySelector('.agile-board').classList.remove('fade');
+
+        setTimeout(() => {
+          issueOpened.classList.remove('hidden');
+          clone.parentNode.removeChild(clone);
+          // window.document.body.parentNode.classList.remove('fixed');
+          clone = null;
+          issueOpened = null;
+
+          issueOpening = false;
+        }, 350);
+      }, 350);
+    }, 350);
   };
 
   const toggleIssue = (issueCard) => {
@@ -157,6 +215,10 @@
   };
 
   const handleLockChanges = () => {
+    if (!window.document.querySelector('.lock')) {
+      return;
+    }
+
     const observer = new MutationObserver(() => {
       if (window.document.querySelector('.lock').style.display !== 'none') {
         return;
@@ -189,13 +251,73 @@
       success: (data) => {
         changeRefreshStatus(true);
 
-        const html = data.replace(/name="authenticity_token" value=".*?"/g, 'name="authenticity_token" value="null"');
+        const dataHtml = data
+          .replace(/name="authenticity_token" value=".*?"/g, 'name="authenticity_token" value="null"')
 
-        if (html === lastRefreshHTML) {
+        if (dataHtml === lastRefreshHTML) {
           return;
         }
 
-        lastRefreshHTML = html;
+        lastRefreshHTML = dataHtml;
+
+        const html = dataHtml
+          .replace(/\n/g, '')
+          .replace(/<b>SP API<\/b>:.*?<br>/g, '')
+          .replace(/<b>Avancement<\/b>:.*?<br>/g, '')
+          .replace(/<span class="hours">\(([0-9]+)sp\)<\/span>/g, '<span class="hours">$1</span>')
+          .replace(/<span class="hours">\((.*?).00h\/0sp\)<\/span>/g, '<span class="hours">$1h</span>')
+          .replace(/<span class="hours">\((.*?).00h\)<\/span>/g, '<span class="hours">$1h</span>')
+          .replace(/<span class="hours">\((.*?)h\/0sp\)<\/span>/g, '<span class="hours">$1h</span>')
+          .replace('data-column-id="1">Nouveau (', 'data-column-id="1">A faire (')
+          .replace('data-column-id="2">Analyse (', 'data-column-id="2">Conception (')
+          .replace('data-column-id="3">Traitement (', 'data-column-id="3">En cours (')
+          .replace('data-column-id="5">Prêt à déployer (', 'data-column-id="5">Awesome (')
+          .replace(/class="fields"(.*?)class="quick-comment"/g, (searchFields) => {
+            const members = [];
+
+            const fieldsHtml = searchFields
+              .replace(/<b>Revue de code<\/b>:(.*?)<br>/g, (searchMembers) => {
+                searchMembers.replace(/<a.*?>(.*?)<\/a>/g, (a, searchMember) => {
+                  const member = searchMember.trim();
+                  if (members.indexOf(member) < 0) {
+                    members.push(member);
+                  }
+                });
+
+                return '';
+              })
+              .replace(/<b>Recette fonctionnelle<\/b>:(.*?)<br>/g, (searchMembers) => {
+                searchMembers.replace(/<a.*?>(.*?)<\/a>/g, (a, searchMember) => {
+                  const member = searchMember.trim();
+                  if (members.indexOf(member) < 0) {
+                    members.push(member);
+                  }
+                });
+
+                return '';
+              });
+
+            if (!members.length) {
+              return fieldsHtml;
+            }
+
+            return fieldsHtml.replace(
+              /<p class="info assigned-user"(.*?)<\/p>/g,
+              (searchUsers) => searchUsers.replace('</p>', () => {
+                const membersHtml = members.map((member) => users[member]
+                  ? `<span class="user">
+                    <img alt="" title="" class="gravatar" src="${users[member]}">
+                  </span>`
+                  : ''
+                ).join('');
+
+                return `${membersHtml}</p>`;
+              })
+            );
+          })
+          .replace(/default=/g, 'default=robohash')
+          .replace(/(;|&)size=.*?&/g, '$1size=100&');
+
         $('#content').html(html);
       },
       fail: () => {
@@ -204,8 +326,8 @@
       complete: () => {
         window.document.querySelector('.agile-board.autoscroll').scrollTop = lastScrollPosition;
 
+        handleLockChanges();
         handleIssuesEvents();
-        fixGravatars();
       },
     });
 
@@ -213,10 +335,11 @@
   };
 
   injectFonts();
+  injectBody();
   injectRefreshStatusComponent();
+  extractUsers();
   handleLockChanges();
   handleIssuesEvents();
-  fixGravatars();
 
   startRefresh();
 })(this);
